298127fbd679763cd4106da5a2e3e024caa974ee
Julia Mathilde Frenning <156354804+Lolrandomthings@users.noreply.github.com>
2025-09-04 09:15:31 +0200

    JavaScript is hell sometimes pt2

--- js.js
+++ js.js
@@ -6,34 +6,43 @@ let submitEl = document.getElementById("submitBtn")
 let scrollEl = document.getElementById("scrollMenu")
 let sizeEl = document.getElementById("textHeigt")
 
+let CorrectColor = ["#b7d4e1", "#00aeff", "#00aaff", "#000000", "#FFFFFF", "#005885"]
+
+
 // Function to change background color
-function changeColorS(e) {
+
+function changeColors(e) {
     e.preventDefault()
-    let changeColorS = colorEl.value
-    console.log("Color")
+    let changeColors = colorEl.value.trim()
+    console.log(changeColors)
     
-    if (CSS.supports('color', changeColorS)) {
-        bodyEl.style.backgroundColor = changeColorS
+    if (CSS.supports('color', changeColors)) {
+        bodyEl.style.backgroundColor = changeColors
+
+        if (CorrectColor.map(c => c.toLowerCase()).includes(changeColors.toLowerCase())) {
+            console.log("Correct color!")
+        }
     } else {
         colorEl.style.color = "white"
-    }   
+    }
 }
 
-submitEl.addEventListener("click", changeColorS)
+
+submitEl.addEventListener("click", changeColors)
 inputEl.addEventListener("input", changeText)
 
 
 function changeText() {
     let textChange = inputEl.value;
     textEl.innerText = textChange;
-    console.log("NewText")
+    console.log("text")
 }
 
 scrollEl.addEventListener("change", changeTextTag)
 
 function changeTextTag(){
     let selectedTag = scrollEl.value;
-    console.log("Size")
+    console.log(scrollEl.value)
 
     let newEl = document.createElement(selectedTag)
     newEl.id = "text"
